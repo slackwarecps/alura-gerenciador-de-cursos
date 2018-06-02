cb07ffe663a772e0f1d2cf56f25050d646dd8e6b
fabioalvaro <fabioalvaro@fabiothundercat.assertiva>
2018-06-02 07:24:21 -0300

    upload inicial

---
+++ src/main/java/br/com/alura/TestandoListas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.alura;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fabioalvaro
+ */
+public class TestandoListas {
+    public static void main(String[] args) {
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a classe aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+        
+        
+        ArrayList<String> aulas = new ArrayList<>();
+        
+        
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+        
+        System.out.println("Listas ");
+    }
+}
